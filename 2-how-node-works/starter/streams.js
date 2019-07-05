616044c1e75f36e8426ba0ab151863f151ee8ba2
Roth <rtt66126@r1stt02.tuttlingen.cn.rt.iiru.de>
2019-07-05 12:35:29 +0200

    finished Node JS behinde the Scenes Section

---
+++ 2-how-node-works/starter/streams.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const server = require("http").createServer();
+
+server.on("request", (req, res) => {
+  // Solution 1
+  // fs.readFile("test-file.txt", (err, data) => {
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+
+  // Solution 2
+  // const readable = fs.createReadStream("test-file.txt");
+  // readable.on("data", chunk => {
+  //   res.write(chunk);
+  // });
+  // readable.on("end", () => {
+  //   res.end();
+  // });
+  // readable.on("error", err => {
+  //   console.log(err);
+  //   res.statusCode = 500;
+  //   res.end("File not found!");
+  // });
+
+  // Solution 2.5
+  // const readable = fs.createReadStream("test-file.txt");
+  // readable.on("data", chunk => {
+  //   if (!res.write(chunk)) {
+  //     console.log("backpressure");
+  //     readable.pause();
+  //   } else {
+  //     res.write(chunk);
+  //   }
+  // });
+  // readable.on("end", () => {
+  //   res.end();
+  // });
+  // readable.on("error", err => {
+  //   console.log(err);
+  //   res.statusCode = 500;
+  //   res.end("File not found!");
+  // });
+
+  // res.on("drain", () => {
+  //   console.log("drained");
+  //   readable.resume();
+  // });
+
+  // Solution 3
+  const readable = fs.createReadStream("test-file.txt");
+
+  // readableSource.pipe(writeableDest)
+  readable.pipe(res);
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening...");
+});
