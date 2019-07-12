d884c29c7bc55220ab026fbc674c16b7870225fa
Roth <rtt66126@r1stt02.tuttlingen.cn.rt.iiru.de>
2019-07-12 12:29:50 +0200

    91. Deleting Documents - done

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -5,18 +5,14 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  checkID,
-  checkBody,
 } = require('./../controllers/tourController')
 
 const router = express.Router()
 
-router.param('id', checkID)
-
 router
   .route('/')
   .get(getAllTours)
-  .post(checkBody, createTour)
+  .post(createTour)
 
 router
   .route(`/:id`)
