5c6243df75698356f83edd1f60163087c9999a63
Roth <rtt66126@r1stt02.tuttlingen.cn.rt.iiru.de>
2019-07-15 15:54:49 +0200

    98. - Pagination | done

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -2,8 +2,63 @@ const Tour = require('./../models/tourModel.js')
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find()
+    console.log(req.query)
 
+    // BUILD QUERY
+    // 1A) Filtering
+    const queryObj = { ...req.query }
+    const exludedFields = ['page', 'sort', 'limit', 'fields']
+    exludedFields.forEach(el => delete queryObj[el])
+
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+
+    let query = Tour.find(JSON.parse(queryStr))
+    // save Query Response after appling filters -> for pagination
+    const documentFilterCount = await Tour.find(JSON.parse(queryStr))
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ')
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort('-createdAt')
+    }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ')
+      query = query.select(fields) // <-- projecting
+    } else {
+      query = query.select('-__v')
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit
+
+    query = query.skip(skip).limit(limit)
+
+    // Check if enough data exists for page
+    if (req.query.page) {
+      // const numTours = await Tour.countDocuments() <-- Orginal Jonas solution, - doesn't consider chainching document count, because of filter.
+      const numTours = documentFilterCount.length
+      console.log(documentFilterCount.length)
+      if (skip >= numTours) throw new Error('This page does not exist')
+    }
+
+    // EXECUTE QUERY
+    const tours = await query
+
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy')
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -54,7 +109,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: err,
     })
   }
 }
